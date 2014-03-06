dd8a4e5c60f3e7e868a6f5293f8028eb76af1730
Rect <rectvv@gmail.com>
2014-03-06 22:52:17 +0800

    add apk file

---
+++ buildAPK/shareModule/umeng_apk.bat
@@ -0,0 +1,1 @@
+"D:\flashbuilder\Adobe Flash Builder 4.6\sdks\AIR3.5\bin\RDT"  -package -target apk -storetype pkcs12 -keystore a.p12 -storepass 1234 umeng.apk UmengSample-app.xml UmengSample.swf  socialize_data socialize_data2 -extdir .
